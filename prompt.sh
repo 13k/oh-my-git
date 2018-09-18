b9f7bb78db4c8d754e38e588a1f735089d312c5c
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2018-09-18 13:34:07 -0300

    Refactor prompt generation:
    * Remove base.sh
    * Fix naming, formatting, missing quotes, linter errors
    * Fix PROMPT_COMMAND being set multiple times
    * Inject a non-printable string in the prompt to mark the prompt as
      being generated by omg
    * Detect original prompt by checking the mark
    * With the original prompt detection, it's now possible to have a
      previous PROMPT_COMMAND that dynamically sets PS1 or have a static
      PS1

--- prompt.sh
+++ prompt.sh
@@ -1,10 +1,38 @@
 [[ -z "$BASH_VERSION" ]] && return 0
 
-OMG_PC_ORIG="$PROMPT_COMMAND"
-OMG_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OMG_MARK="omg_prompt_mark"
+OMG_ESC_MARK="\\eX${OMG_MARK}\\e\\\\"
+OMG_ORIGINAL_PS1="$PS1"
 
-source "${OMG_DIR}/base.sh"
+# colors
+omg_reset='\[\e[0m\]'
+omg_black='\[\e[0;30m\]'
+omg_red='\[\e[0;31m\]'
+omg_green='\[\e[0;32m\]'
+omg_yellow='\[\e[0;33m\]'
+omg_blue='\[\e[0;34m\]'
+omg_purple='\[\e[0;35m\]'
+omg_cyan='\[\e[0;36m\]'
+omg_white='\[\e[0;37m\]'
+omg_white_bold='\[\e[1;37m\]'
+omg_bg_black='\[\e[40m\]'
+omg_bg_red='\[\e[41m\]'
+omg_bg_green='\[\e[42m\]'
+omg_bg_yellow='\[\e[43m\]'
+omg_bg_blue='\[\e[44m\]'
+omg_bg_purple='\[\e[45m\]'
+omg_bg_cyan='\[\e[46m\]'
+omg_bg_white='\[\e[47m\]'
+omg_black_on_white="${omg_black}${omg_bg_white}"
+omg_yellow_on_white="${omg_yellow}${omg_bg_white}"
+omg_purple_on_white="${omg_purple}${omg_bg_white}"
+omg_red_on_white="${omg_red}${omg_bg_white}"
+omg_red_on_black="${omg_red}${omg_bg_black}"
+omg_black_on_red="${omg_black}${omg_bg_red}"
+omg_white_on_red="${omg_white}${omg_bg_red}"
+omg_yellow_on_red="${omg_yellow}${omg_bg_red}"
 
+# config
 : ${omg_separator_symbol:=''}
 : ${omg_terminator_symbol:="$omg_separator_symbol"}
 : ${omg_is_a_git_repo_symbol:=''}
@@ -26,159 +54,275 @@ source "${OMG_DIR}/base.sh"
 : ${omg_should_push_symbol:=''}                #    
 : ${omg_has_stashes_symbol:=''}
 
-: ${omg_default_color_on:='\[\033[1;37m\]'}
-: ${omg_default_color_off:='\[\033[0m\]'}
-: ${omg_terminator_color:='\[\e[0;31m\e[40m\]'}
+: ${omg_default_color_on:="$omg_white_bold"}
+: ${omg_default_color_off:="$omg_reset"}
+: ${omg_terminator_color:="$omg_red_on_black"}
 
 : ${omg_termination:="${omg_terminator_color}${omg_terminator_symbol}"}
 
+function omg_callback_defined() {
+  declare -fF "$1" &> /dev/null
+}
+
+function omg_eval_prompt_callback() {
+  local callback_name="omg_prompt_callback"
+  [[ -n "$1" ]] && callback_name+="_$1"
+  omg_callback_defined "$callback_name" && "$callback_name"
+}
+
 function omg_enrich_append() {
-  local flag=$1
-  local symbol=$2
-  local color=${3:-$omg_default_color_on}
-  if [[ $flag == false ]]; then symbol=' '; fi
+  local flag="$1"
+  local symbol="$2"
+  local color="${3:-$omg_default_color_on}"
+  [[ $flag == false ]] && symbol=' '
   echo -n "${color}${symbol}  "
 }
 
-function omg_custom_build_prompt() {
-  local enabled=${1}
-  local current_commit_hash=${2}
-  local is_a_git_repo=${3}
-  local current_branch=$4
-  local detached=${5}
-  local just_init=${6}
-  local has_upstream=${7}
-  local has_modifications=${8}
-  local has_modifications_cached=${9}
-  local has_adds=${10}
-  local has_deletions=${11}
-  local has_deletions_cached=${12}
-  local has_untracked_files=${13}
-  local ready_to_commit=${14}
-  local tag_at_current_commit=${15}
-  local is_on_a_tag=${16}
-  local has_upstream=${17}
-  local commits_ahead=${18}
-  local commits_behind=${19}
-  local has_diverged=${20}
-  local should_push=${21}
-  local will_rebase=${22}
-  local has_stashes=${23}
+function omg_detect_state() {
+  local enabled="$OMG_PROMPT_ENABLED"
 
-  local prompt=""
-  local original_prompt=$PS1
+  if [[ -z "$enabled" ]]; then
+		enabled="$(git config --get oh-my-git.enabled 2> /dev/null)"
+	fi
 
-  local reset='\[\e[0m\]'
+  [[ $enabled == false ]] && return 1
 
-  # foreground
-  local black='\[\e[0;30m\]'
-  local red='\[\e[0;31m\]'
-  local green='\[\e[0;32m\]'
-  local yellow='\[\e[0;33m\]'
-  local blue='\[\e[0;34m\]'
-  local purple='\[\e[0;35m\]'
-  local cyan='\[\e[0;36m\]'
-  local white='\[\e[0;37m\]'
+	local is_git_repo=false
+	local current_commit_hash
+	local current_branch
+	local is_detached=false
+	local just_init=false
+	local upstream
+	local has_upstream=false
+	local has_modifications=false
+	local has_modifications_cached=false
+	local has_adds=false
+	local has_deletions=false
+	local has_deletions_cached=false
+	local has_untracked_files=false
+	local ready_to_commit=false
+	local tag_at_current_commit
+	local is_on_a_tag=false
+	local has_upstream=false
+	local commits_ahead
+	local commits_behind
+	local has_diverged=false
+	local should_push=false
+	local will_rebase=false
+	local has_stashes=false
 
-  # background
-  local background_black='\[\e[40m\]'
-  local background_red='\[\e[41m\]'
-  local background_green='\[\e[42m\]'
-  local background_yellow='\[\e[43m\]'
-  local background_blue='\[\e[44m\]'
-  local background_purple='\[\e[45m\]'
-  local background_cyan='\[\e[46m\]'
-  local background_white='\[\e[47m\]'
+  current_commit_hash="$(git rev-parse HEAD 2> /dev/null)"
+	[[ -n "$current_commit_hash" ]] && is_git_repo=true
 
-  # fg+bg
-  local black_on_white="${black}${background_white}"
-  local yellow_on_white="${yellow}${background_white}"
-  local purple_on_white="${purple}${background_white}"
-  local red_on_white="${red}${background_white}"
-  local red_on_black="${red}${background_black}"
-  local black_on_red="${black}${background_red}"
-  local white_on_red="${white}${background_red}"
-  local yellow_on_red="${yellow}${background_red}"
+  if [[ $is_git_repo == true ]]; then
+    current_branch="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
+    [[ "$current_branch" == "HEAD" ]] && is_detached=true
 
-  # flags
-  local omg_default_color_on="${black_on_white}"
+		local number_of_logs
+    number_of_logs="$(git log --pretty=oneline -n1 2> /dev/null | wc -l)"
 
-  if [[ $is_a_git_repo == true ]]; then
+    if [[ $number_of_logs -eq 0 ]]; then
+      just_init=true
+    else
+			upstream="$(git rev-parse --symbolic-full-name --abbrev-ref "@{upstream}" 2> /dev/null)"
+      [[ -n "${upstream}" && "${upstream}" != "@{upstream}" ]] && has_upstream=true
+
+      local git_status
+			git_status="$(git status --porcelain 2> /dev/null)"
+
+      [[ "$git_status" =~ ($'\n'|^).M ]] && has_modifications=true
+      [[ "$git_status" =~ ($'\n'|^)M ]] && has_modifications_cached=true
+      [[ "$git_status" =~ ($'\n'|^)A ]] && has_adds=true
+      [[ "$git_status" =~ ($'\n'|^).D ]] && has_deletions=true
+      [[ "$git_status" =~ ($'\n'|^)D ]] && has_deletions_cached=true
+      [[ "$git_status" =~ ($'\n'|^)[MAD] && ! "$git_status" =~ ($'\n'|^).[MAD\?] ]] && ready_to_commit=true
+
+      local number_of_untracked_files
+			number_of_untracked_files="$(\grep -c "^??" <<< "${git_status}")"
+      [[ $number_of_untracked_files -gt 0 ]] && has_untracked_files=true
+
+			tag_at_current_commit="$(git describe --exact-match --tags "$current_commit_hash" 2> /dev/null)"
+      [[ -n "$tag_at_current_commit" ]] && is_on_a_tag=true
+
+			local commits_diff
+      if [[ $has_upstream == true ]]; then
+				commits_diff="$(git log --pretty=oneline --topo-order --left-right "${current_commit_hash}...${upstream}" 2> /dev/null)"
+				commits_ahead="$(\grep -c "^<" <<< "$commits_diff")"
+				commits_behind="$(\grep -c "^>" <<< "$commits_diff")"
+      fi
+
+      [[ $commits_ahead -gt 0 && $commits_behind -gt 0 ]] && has_diverged=true
+      [[ $has_diverged == false && $commits_ahead -gt 0 ]] && should_push=true
+
+			local number_of_stashes
+      will_rebase="$(git config --get "branch.${current_branch}.rebase" 2> /dev/null)"
+      number_of_stashes="$(git stash list -n1 2> /dev/null | wc -l)"
+      [[ $number_of_stashes -gt 0 ]] && has_stashes=true
+    fi
+  fi
+
+	echo "$is_git_repo"
+	echo "$current_commit_hash"
+	echo "$current_branch"
+	echo "$is_detached"
+	echo "$just_init"
+	echo "$upstream"
+	echo "$has_upstream"
+	echo "$has_modifications"
+	echo "$has_modifications_cached"
+	echo "$has_adds"
+	echo "$has_deletions"
+	echo "$has_deletions_cached"
+	echo "$has_untracked_files"
+	echo "$ready_to_commit"
+	echo "$tag_at_current_commit"
+	echo "$is_on_a_tag"
+	echo "$has_upstream"
+	echo "$commits_ahead"
+	echo "$commits_behind"
+	echo "$has_diverged"
+	echo "$should_push"
+	echo "$will_rebase"
+	echo "$has_stashes"
+}
+
+function omg_previous_prompt() {
+	if [[ "$PS1" == *${OMG_MARK}* ]]; then
+		echo "$OMG_ORIGINAL_PS1"
+	else
+		echo "$PS1"
+	fi
+}
+
+function omg_build_prompt() {
+	local -a state_array
+	local prev_prompt
+	prev_prompt="$(omg_previous_prompt)"
+
+	if ! readarray -t state_array <<< "$(omg_detect_state)"; then
+		echo "$prev_prompt"
+		return 0
+	fi
+
+	local -a state_vars=(
+		"is_git_repo"
+		"current_commit_hash"
+		"current_branch"
+		"is_detached"
+		"just_init"
+		"upstream"
+		"has_upstream"
+		"has_modifications"
+		"has_modifications_cached"
+		"has_adds"
+		"has_deletions"
+		"has_deletions_cached"
+		"has_untracked_files"
+		"ready_to_commit"
+		"tag_at_current_commit"
+		"is_on_a_tag"
+		"has_upstream"
+		"commits_ahead"
+		"commits_behind"
+		"has_diverged"
+		"should_push"
+		"will_rebase"
+		"has_stashes"
+	)
+
+	if [[ ${#state_array[@]} -ne ${#state_vars[@]} ]]; then
+		echo "$prev_prompt"
+		return 0
+	fi
+
+	local state_var state_var_idx=0
+	for state_var in "${state_vars[@]}"; do
+		local "$state_var"="${state_array[$((state_var_idx++))]}"
+	done
+
+  local prompt="\\[$OMG_ESC_MARK\\]"
+
+  if [[ $is_git_repo == true ]]; then
     # on filesystem
-    prompt="$(omg_eval_prompt_callback before_first)"
-    prompt+="${black_on_white} "
-    prompt+=$(omg_enrich_append $is_a_git_repo $omg_is_a_git_repo_symbol "${black_on_white}")
-    prompt+=$(omg_enrich_append $has_stashes $omg_has_stashes_symbol "${red_on_white}")
-
-    prompt+=$(omg_enrich_append $has_untracked_files $omg_has_untracked_files_symbol "${red_on_white}")
-    prompt+=$(omg_enrich_append $has_modifications $omg_has_modifications_symbol "${red_on_white}")
-    prompt+=$(omg_enrich_append $has_deletions $omg_has_deletions_symbol "${red_on_white}")
-
+    prompt+="$(omg_eval_prompt_callback before_first)"
+    prompt+="${omg_black_on_white} "
+    prompt+="$(omg_enrich_append "$is_git_repo" "$omg_is_a_git_repo_symbol" "${omg_black_on_white}")"
+    prompt+="$(omg_enrich_append "$has_stashes" "$omg_has_stashes_symbol" "${omg_red_on_white}")"
+    prompt+="$(omg_enrich_append "$has_untracked_files" "$omg_has_untracked_files_symbol" "${omg_red_on_white}")"
+    prompt+="$(omg_enrich_append "$has_modifications" "$omg_has_modifications_symbol" "${omg_red_on_white}")"
+    prompt+="$(omg_enrich_append "$has_deletions" "$omg_has_deletions_symbol" "${omg_red_on_white}")"
     # ready
-    prompt+=$(omg_enrich_append $has_adds $omg_has_adds_symbol "${black_on_white}")
-    prompt+=$(omg_enrich_append $has_modifications_cached $omg_has_cached_modifications_symbol "${black_on_white}")
-    prompt+=$(omg_enrich_append $has_deletions_cached $omg_has_cached_deletions_symbol "${black_on_white}")
-
+    prompt+="$(omg_enrich_append "$has_adds" "$omg_has_adds_symbol" "${omg_black_on_white}")"
+    prompt+="$(omg_enrich_append "$has_modifications_cached" "$omg_has_cached_modifications_symbol" "${omg_black_on_white}")"
+    prompt+="$(omg_enrich_append "$has_deletions_cached" "$omg_has_cached_deletions_symbol" "${omg_black_on_white}")"
     # next operation
-    prompt+=$(omg_enrich_append $ready_to_commit $omg_ready_to_commit_symbol "${red_on_white}")
-
+    prompt+="$(omg_enrich_append "$ready_to_commit" "$omg_ready_to_commit_symbol" "${omg_red_on_white}")"
     # where
-    prompt="${prompt} ${white_on_red}${omg_separator_symbol} ${black_on_red}"
+    prompt+=" ${omg_white_on_red}${omg_separator_symbol} ${omg_black_on_red}"
 
-    if [[ $detached == true ]]; then
-      prompt+=$(omg_enrich_append $detached $omg_detached_symbol "${white_on_red}")
-      prompt+=$(omg_enrich_append $detached "(${current_commit_hash:0:7})" "${black_on_red}")
+    if [[ $is_detached == true ]]; then
+      prompt+="$(omg_enrich_append "$is_detached" "$omg_detached_symbol" "$omg_white_on_red")"
+      prompt+="$(omg_enrich_append "$is_detached" "(${current_commit_hash:0:7})" "$omg_black_on_red")"
     else
       if [[ $has_upstream == false ]]; then
-        prompt+=$(omg_enrich_append true "-- ${omg_not_tracked_branch_symbol}  --  (${current_branch})" "${black_on_red}")
+        prompt+="$(omg_enrich_append true "-- ${omg_not_tracked_branch_symbol}  --  (${current_branch})" "${omg_black_on_red}")"
       else
+				local type_of_upstream
+
         if [[ $will_rebase == true ]]; then
-          local type_of_upstream=$omg_rebase_tracking_branch_symbol
+          type_of_upstream="$omg_rebase_tracking_branch_symbol"
         else
-          local type_of_upstream=$omg_merge_tracking_branch_symbol
+          type_of_upstream="$omg_merge_tracking_branch_symbol"
         fi
 
         if [[ $has_diverged == true ]]; then
-          prompt+=$(omg_enrich_append true "-${commits_behind} ${omg_has_diverged_symbol} +${commits_ahead}" "${white_on_red}")
+          prompt+="$(omg_enrich_append true "-${commits_behind} ${omg_has_diverged_symbol} +${commits_ahead}" "${omg_white_on_red}")"
         else
           if [[ $commits_behind -gt 0 ]]; then
-            prompt+=$(omg_enrich_append true "-${commits_behind} ${white_on_red}${omg_can_fast_forward_symbol}${black_on_red} --" "${black_on_red}")
+            prompt+="$(omg_enrich_append true "-${commits_behind} ${omg_white_on_red}${omg_can_fast_forward_symbol}${omg_black_on_red} --" "${omg_black_on_red}")"
           fi
 
           if [[ $commits_ahead -gt 0 ]]; then
-            prompt+=$(omg_enrich_append true "-- ${white_on_red}${omg_should_push_symbol}${black_on_red}  +${commits_ahead}" "${black_on_red}")
+            prompt+="$(omg_enrich_append true "-- ${omg_white_on_red}${omg_should_push_symbol}${omg_black_on_red}  +${commits_ahead}" "${omg_black_on_red}")"
           fi
 
-          if [[ $commits_ahead == 0 && $commits_behind == 0 ]]; then
-            prompt+=$(omg_enrich_append true " --   -- " "${black_on_red}")
+          if [[ $commits_ahead -eq 0 && $commits_behind -eq 0 ]]; then
+            prompt+="$(omg_enrich_append true " --   -- " "${omg_black_on_red}")"
           fi
         fi
 
-        prompt+=$(omg_enrich_append true "(${current_branch} ${type_of_upstream} ${upstream//\/$current_branch/})" "${black_on_red}")
+        prompt+="$(omg_enrich_append true "(${current_branch} ${type_of_upstream} ${upstream//\/$current_branch/})" "${omg_black_on_red}")"
       fi
     fi
 
-    prompt+=$(omg_enrich_append ${is_on_a_tag} "${omg_is_on_a_tag_symbol} ${tag_at_current_commit}" "${black_on_red}")
+    prompt+=$(omg_enrich_append "${is_on_a_tag}" "${omg_is_on_a_tag_symbol} ${tag_at_current_commit}" "${omg_black_on_red}")
     prompt+="$(omg_eval_prompt_callback after_first)"
-    prompt+="${omg_termination}${reset}"
-    prompt+="\n"
+    prompt+="${omg_termination}${omg_reset}"
+    prompt+="\\n"
     prompt+="$(omg_eval_prompt_callback before_second)"
-    prompt+="${PS1}"
+    prompt+="${prev_prompt}"
     prompt+="$(omg_eval_prompt_callback after_second)"
   else
     prompt+="$(omg_eval_prompt_callback before_second)"
-    prompt+="${PS1}"
+    prompt+="${prev_prompt}"
     prompt+="$(omg_eval_prompt_callback after_second)"
   fi
 
-  echo "${prompt}"
+  echo "$prompt"
 }
 
-function omg_bash_prompt() {
-  ret=$?
-  PS1="$(omg_build_prompt)"
+function omg_prompt_command() {
+  local ret=$?
+	PS1="$(omg_build_prompt)"
   return $ret
 }
 
-export PS2="${yellow}→${reset} "
-export PROMPT_COMMAND="${OMG_PC_ORIG}${OMG_PC_ORIG:+"; "}omg_bash_prompt"
+function omg_prompt_init() {
+	[[ "$PROMPT_COMMAND" =~ omg_prompt_command ]] && return 0
+
+	local prompt_cmd="$PROMPT_COMMAND"
+	export PS2="${omg_yellow}→${omg_reset} "
+	export PROMPT_COMMAND="${prompt_cmd}${prompt_cmd:+"; "}omg_prompt_command"
+}
+
+omg_prompt_init
